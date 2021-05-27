14f6672c154a29c21ce16c6bf66cc95c723abaed
Bianca Barria <bianca.barria@beetrack.com>
2021-05-27 18:01:00 -0400

    feat: controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  #Route structure for route to controller function
+  get 'hello', to: 'pages#contact'
+  get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
